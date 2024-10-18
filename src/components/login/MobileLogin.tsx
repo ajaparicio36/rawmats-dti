ae4925f5b0d141c41a1f822459d6567cc945427f
AJ APARICIO <aparicio.anthonyjohn@gmail.com>
2024-10-18 19:03:20 +0800

    style: change responsive design to wider screens

--- src/components/login/MobileLogin.tsx
+++ src/components/login/MobileLogin.tsx
@@ -1,11 +1,22 @@
 import React from "react";
 import LoginForm from "./LoginForm";
+import Image from "next/image";
+import logo from "../../public/logo.png";
 
 const MobileLogin = () => {
   return (
-    <div className="bg-white p-6 rounded-lg shadow-md w-full max-w-sm mx-auto">
-      <h1 className="text-xl font-bold mb-4 text-center">Mobile Login</h1>
-      <LoginForm />
+    <div className="flex flex-col h-screen">
+      <div className="flex h-3/5 items-center justify-center text-center">
+        <Image
+          src={logo}
+          alt="RAWMATS Logo"
+          width={400}
+          className="max-w-full h-auto mx-auto"
+        />
+      </div>
+      <div className="flex flex-1 bg-white p-6 rounded-t-md shadow-md">
+        <LoginForm />
+      </div>
     </div>
   );
 };
