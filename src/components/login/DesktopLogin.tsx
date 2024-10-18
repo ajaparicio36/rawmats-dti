b5cd363476aba163ed604d6a4218eb13a8e062c5
AJ APARICIO <aparicio.anthonyjohn@gmail.com>
2024-10-18 19:04:29 +0800

    style: change design to reflect on medium and larger screens

--- src/components/login/DesktopLogin.tsx
+++ src/components/login/DesktopLogin.tsx
@@ -1,11 +1,26 @@
 import React from "react";
 import LoginForm from "./LoginForm";
+import Image from "next/image";
+import logo from "../../public/logo.png";
 
 const DesktopLogin = () => {
   return (
-    <div className="bg-white p-8 rounded-lg shadow-md w-96">
-      <h1 className="text-2xl font-bold mb-6 text-center">Desktop Login</h1>
-      <LoginForm />
+    <div className="max-w-6xl m-4 w-full h-3/4 flex items-center justify-center bg-white rounded-2xl shadow-xl shadow-rawmats-secondary-700 overflow-hidden">
+      <div className="flex flex-col justify-center gap-4 items-start w-3/4 p-12">
+        <h2 className="text-5xl font-bold mb-2 text-rawmats-text-700">
+          Ready to Dive In?
+        </h2>
+        <p className="text-rawmats-text-500 mb-8">Login to your account!</p>
+        <LoginForm />
+      </div>
+      <div className="w-1/2 h-full bg-rawmats-secondary-100 p-12 flex items-center justify-center">
+        <Image
+          src={logo}
+          alt="RAWMATS Logo"
+          width={400}
+          className="max-w-full h-auto"
+        />
+      </div>
     </div>
   );
 };
