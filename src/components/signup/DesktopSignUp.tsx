c7a255043df0abf0462703e34c19fd5943386f24
franzsamilo <franzeliezer.samilo-22@cpu.edu.ph>
2024-10-29 01:13:12 +0800

    fix: shadow color for desktop sign up

--- src/components/signup/DesktopSignUp.tsx
+++ src/components/signup/DesktopSignUp.tsx
@@ -5,7 +5,7 @@ import logo from "../../public/logo.png";
 
 const SignUp = () => {
   return (
-    <div className="max-w-6xl w-full h-[80vh] flex items-center justify-center bg-white rounded-2xl shadow-2xl overflow-hidden">
+    <div className="max-w-6xl w-full h-[80vh] flex items-center justify-center bg-white rounded-2xl shadow-2xl shadow-rawmats-secondary-700 overflow-hidden">
       <div className="flex flex-col justify-center gap-4 items-start w-1/2 p-12 overflow-y-auto h-full scrollbar-hide">
         <h2 className="lg:text-5xl lg:mt-12 text-4xl font-bold text-rawmats-text-700 mt-12">
           SIGN UP NOW!
