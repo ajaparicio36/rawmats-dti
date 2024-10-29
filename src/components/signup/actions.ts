b90bb3680a222e1e67d95d9467cd05e4ed8289e7
franzsamilo <franzeliezer.samilo-22@cpu.edu.ph>
2024-10-29 17:42:12 +0800

    feat: storing phone as metadata (currently doesn't show in Supabase auth)

--- src/components/signup/actions.ts
+++ src/components/signup/actions.ts
@@ -54,7 +54,7 @@ export async function signup(formData: FormData) {
       options: {
         data: {
           display_name: name,
-          phone,
+          phone: phone,
         },
       },
     });
