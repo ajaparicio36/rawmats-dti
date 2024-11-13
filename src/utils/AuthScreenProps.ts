57fe435c4079c8e14311419b50492a1e39239d36
AJ Aparicio <anthonyjohn.aparicio-22@cpu.edu.ph>
2024-11-14 02:24:17 +0800

    refactor: convert supabase actions to handlers

---
+++ src/utils/AuthScreenProps.ts
@@ -0,0 +1,8 @@
+export interface AuthScreenProps {
+  header: string;
+  message: string;
+  body: React.ReactNode;
+  mobileHeader?: string;
+  mobileMessage?: string;
+  mobileBody?: React.ReactNode;
+}
