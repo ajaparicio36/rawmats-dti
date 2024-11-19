a764902fbe26a7f3e61d01d7f68164114cf859d8
Jeff Andre Millan <122857926+jeffandre123@users.noreply.github.com>
2024-11-19 20:56:51 +0800

    feat: frontend design for Product info page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,9 @@ import React from "react";
 import { redirect } from "next/navigation";
 import { createClient } from "@/utils/supabase/server";
 import SignoutButton from "@/components/AuthComponents/SignoutButton";
+import ProductPreviewCard from "@/components/Products/ProductPreviewCard";
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
+import prisma from "@/utils/prisma/client";
 
 const Home = async () => {
   const supabase = createClient();
@@ -10,9 +13,31 @@ const Home = async () => {
   if (error || !data.user) {
     redirect("/login");
   }
+
+  const products = await prisma.product.findMany({
+    include: {
+      supplier: true,
+    },
+  });
+
   return (
     <div>
-      Hello {data.user.email}, Click here to <SignoutButton />
+      Hello {data.user.email}! <SignoutButton />
+      <ScrollArea className="w-full whitespace-nowrap rounded-md ">
+        <div className="flex w-max space-x-4 p-4">
+          {products.map((product) => (
+            <ProductPreviewCard
+              key={product.id}
+              id={product.id}
+              name={product.name}
+              price={product.price}
+              supplierName={product.supplier.businessName}
+              location={product.supplier.businessLocation}
+            />
+          ))}
+        </div>
+        <ScrollBar orientation="horizontal" />
+      </ScrollArea>
     </div>
   );
 };
