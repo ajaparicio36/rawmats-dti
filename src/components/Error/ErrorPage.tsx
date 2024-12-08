d596fadaf96016dc612dec0f39cf18e76a724ed2
AJ APARICIO <aparicio.anthonyjohn@gmail.com>
2024-12-08 15:25:40 +0800

    style: change style for text to be clearer

--- src/components/Error/ErrorPage.tsx
+++ src/components/Error/ErrorPage.tsx
@@ -18,15 +18,15 @@ export default function ErrorPage({
   const message = errorMessage || "An unexpected error occurred";
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-background-700">
-      <Card className="w-full max-w-md border-accent-300">
-        <CardHeader className="bg-primary-500 text-primary-foreground">
+    <div className="flex items-center justify-center min-h-screen bg-rawmats-background-700">
+      <Card className="w-full max-w-md border-rawmats-accent-300">
+        <CardHeader className=" text-rawmats-primary-700">
           <CardTitle className="text-4xl font-bold text-center">
             Error {code}
           </CardTitle>
         </CardHeader>
         <CardContent className="mt-6">
-          <p className="text-center text-text-500">{message}</p>
+          <p className="text-center text-rawmats-text-500">{message}</p>
         </CardContent>
         <CardFooter className="flex justify-center mt-6">
           <ErrorButton />
