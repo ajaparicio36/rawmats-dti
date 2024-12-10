b6d1b388015b214014666da4c0d90d2760ee5d65
AJ Aparicio <anthonyjohn.aparicio-22@cpu.edu.ph>
2024-12-11 01:23:37 +0800

    chore: remove console logs

--- src/app/api/supplier/location/route.ts
+++ src/app/api/supplier/location/route.ts
@@ -1,16 +1,10 @@
 import { NextRequest, NextResponse } from "next/server";
 
 function extractCoordinatesFromLink(link: string) {
-  // Regex to match coordinates in the format: /place/lat,long
-  console.log("Link", link);
-
   const coordMatch = link.match(/place\/([^,]+),([^,\/]+)/);
-  console.log("coordMatch", coordMatch);
 
   if (coordMatch) {
     const [, latitude, longitude] = coordMatch;
-    console.log("latitude", latitude);
-    console.log("longitude", longitude);
     return {
       latitude: parseFloat(latitude),
       longitude: parseFloat(longitude),
@@ -25,8 +19,6 @@ export const POST = async (request: NextRequest) => {
     const { locationLink } = await request.json();
     const { latitude, longitude } = extractCoordinatesFromLink(locationLink);
     const API_KEY = process.env.GOOGLE_MAPS_API_KEY as string;
-    console.log(latitude, longitude);
-    console.log(API_KEY);
     const response = await fetch(
       `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=${API_KEY}`,
       {
@@ -38,9 +30,7 @@ export const POST = async (request: NextRequest) => {
     );
 
     const data = await response.json();
-    console.log(data);
     const locationName = data.results[0]?.formatted_address;
-    console.log(locationName);
     return NextResponse.json({ locationName });
   } catch (error) {
     console.error("Error fetching location name:", error);
--- src/components/Home/NavBar.tsx
+++ src/components/Home/NavBar.tsx
@@ -4,6 +4,7 @@ import { Search } from "lucide-react";
 import { Input } from "@/components/ui/input";
 import { SheetMenu } from "./SheetMenu";
 import { User, Supplier } from "@prisma/client";
+import { useRouter } from "next/navigation";
 
 interface NavbarProps {
   user: User;
@@ -11,12 +12,16 @@ interface NavbarProps {
 }
 
 export default function NavBar({ user, supplier }: NavbarProps) {
+  const router = useRouter();
   return (
     <nav className="w-full bg-rawmats-secondary-700 border-b">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         {/* RAWMATS text */}
         <div className="flex justify-center py-4">
-          <h1 className="text-3xl text-white">
+          <h1
+            onClick={() => router.push("/")}
+            className="text-3xl px-2 py-1 rounded-md text-white hover:cursor-pointer hover:bg-rawmats-secondary-900"
+          >
             <span className="font-bold">RAW</span>MATS
           </h1>
         </div>
