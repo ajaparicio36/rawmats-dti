803214933ac372e763ea583ccb730fe3a14a3321
AJ Aparicio <anthonyjohn.aparicio-22@cpu.edu.ph>
2025-01-27 21:00:25 +0800

    feat(sidebar): add account section functionality

--- src/components/Sidebar/SidebarUser.tsx
+++ src/components/Sidebar/SidebarUser.tsx
@@ -1,19 +1,11 @@
 "use client";
 
-import {
-  BadgeCheck,
-  Bell,
-  ChevronsUpDown,
-  CreditCard,
-  LogOut,
-  Sparkles,
-} from "lucide-react";
+import { ChevronsUpDown, LogOut } from "lucide-react";
 
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import {
   DropdownMenu,
   DropdownMenuContent,
-  DropdownMenuGroup,
   DropdownMenuItem,
   DropdownMenuLabel,
   DropdownMenuSeparator,
@@ -25,6 +17,7 @@ import {
   SidebarMenuItem,
   useSidebar,
 } from "@/components/ui/sidebar";
+import { logout } from "../AuthHandlers/LoginHandler";
 
 export function NavUser({
   user,
@@ -36,6 +29,9 @@ export function NavUser({
   };
 }) {
   const { isMobile } = useSidebar();
+  const logOut = async () => {
+    await logout();
+  };
 
   return (
     <SidebarMenu>
@@ -48,7 +44,9 @@ export function NavUser({
             >
               <Avatar className="h-8 w-8 rounded-lg">
                 <AvatarImage src={user.avatar} alt={user.name} />
-                <AvatarFallback className="rounded-lg">CN</AvatarFallback>
+                <AvatarFallback className="rounded-lg">
+                  {user.name[0]}
+                </AvatarFallback>
               </Avatar>
               <div className="grid flex-1 text-left text-sm leading-tight">
                 <span className="truncate font-semibold">{user.name}</span>
@@ -67,7 +65,9 @@ export function NavUser({
               <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
                 <Avatar className="h-8 w-8 rounded-lg">
                   <AvatarImage src={user.avatar} alt={user.name} />
-                  <AvatarFallback className="rounded-lg">CN</AvatarFallback>
+                  <AvatarFallback className="rounded-lg">
+                    {user.name[0]}
+                  </AvatarFallback>
                 </Avatar>
                 <div className="grid flex-1 text-left text-sm leading-tight">
                   <span className="truncate font-semibold">{user.name}</span>
@@ -75,30 +75,9 @@ export function NavUser({
                 </div>
               </div>
             </DropdownMenuLabel>
+
             <DropdownMenuSeparator />
-            <DropdownMenuGroup>
-              <DropdownMenuItem>
-                <Sparkles />
-                Upgrade to Pro
-              </DropdownMenuItem>
-            </DropdownMenuGroup>
-            <DropdownMenuSeparator />
-            <DropdownMenuGroup>
-              <DropdownMenuItem>
-                <BadgeCheck />
-                Account
-              </DropdownMenuItem>
-              <DropdownMenuItem>
-                <CreditCard />
-                Billing
-              </DropdownMenuItem>
-              <DropdownMenuItem>
-                <Bell />
-                Notifications
-              </DropdownMenuItem>
-            </DropdownMenuGroup>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem>
+            <DropdownMenuItem onClick={logOut}>
               <LogOut />
               Log out
             </DropdownMenuItem>
--- src/components/SupplierDashboard/SupplierScreen.tsx
+++ src/components/SupplierDashboard/SupplierScreen.tsx
@@ -8,7 +8,11 @@ import { SupplierSidebar } from "../Sidebar/SupplierSidebar";
 const SupplierScreen: React.FC<SupplierDashboardProps> = ({ supplier }) => {
   return (
     <div className="flex h-screen w-full bg-background">
-      <SupplierSidebar />
+      <SupplierSidebar
+        name={supplier.user.displayName}
+        email={supplier.user.email}
+        avatar=""
+      />
       <SidebarInset className="flex flex-col w-full overflow-hidden">
         <div className="bg-[#A3E6FD]/30 border-b px-8 py-6 flex items-center">
           <SidebarTrigger className="mr-4" />
